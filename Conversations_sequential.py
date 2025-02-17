a77d1490207bcaa7067c2e047294d08f8245a535
Arman Daneshdoost <armandaneshdoost@gmail.com>
2025-02-17 14:07:01 +0330

    AutoGen Conversable Agent added

---
+++ Conversations_sequential.py
@@ -0,0 +1,74 @@
+import os
+from autogen import ConversableAgent
+
+config_list = [{
+    # Let's choose the model
+    "model": "qwen2.5:latest",
+    "base_url": "http://localhost:11434",
+    "api_key": "ollama",
+}]
+
+number_agent = ConversableAgent(
+    name="Number_Agent",
+    system_message="You return me the numbers I give you, one number each line.",
+    llm_config={"config_list": config_list},
+    human_input_mode="NEVER"
+)
+
+adder_agent = ConversableAgent(
+    name="Adder_Agent",
+    system_message="You add 1 to each number I give you and return me the new numbers, one number each line.",
+    llm_config={"config_list": config_list},
+    human_input_mode="NEVER"
+)
+
+multiplier_agent = ConversableAgent(
+    name="Multiplier_Agent",
+    system_message="You multiply each number I give you by 2 and return me the new numbers, one number each line.",
+    llm_config={"config_list": config_list},
+    human_input_mode="NEVER"
+)
+
+subtracter_agent = ConversableAgent(
+    name="Subtracter_Agent",
+    system_message="You subtract 1 from each number I give you and return me the new numbers, one number each line.",
+    llm_config={"config_list": config_list},
+    human_input_mode="NEVER",
+)
+
+divider_agent = ConversableAgent(
+    name="Divider_Agent",
+    system_message="You divide each number I give you by 2 and return me the new numbers, one number each line.",
+    llm_config={"config_list": config_list},
+    human_input_mode="NEVER",
+)
+
+
+chat_results = number_agent.initiate_chats(
+    [
+        {
+            "recipient": adder_agent,
+            "message": "14",
+            "max_turns": 2,
+            "summary_method": "last_msg", #last message of the chat
+        },
+        {
+            "recipient": multiplier_agent,
+            "message": "These are my numbers",
+            "max_turns": 2,
+            "summary_method": "last_msg",
+        },
+        {
+            "recipient": subtracter_agent,
+            "message": "These are my numbers",
+            "max_turns": 2,
+            "summary_method": "last_msg",
+        },
+        {
+            "recipient": divider_agent,
+            "message": "These are my numbers",
+            "max_turns": 2,
+            "summary_method": "last_msg",
+        },
+    ]
+)
